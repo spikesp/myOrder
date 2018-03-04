d2ac15ebf0c3ee5b7cb51b20100385854f7bca7f
spike <weisuojiaoshou@gmail.com>
2018-03-04 16:24:31 +0900

    add record funtion

--- app/controllers/fitness_records_controller.rb
+++ app/controllers/fitness_records_controller.rb
@@ -1,6 +1,22 @@
 class FitnessRecordsController < ApplicationController
   def index
     @fitnessRecords = FitnessRecord.all
-    @fitnessMoves = FitnessMove.all
+  end
+
+  def create
+    @record = FitnessRecord.new(fitness_record_params)
+    if @record
+      @record.save
+      flash[:success] = "成功新增锻炼记录！"
+      redirect_to fitness_records_path
+    else
+      render fitness_records_path
+    end
+  end
+
+  private
+
+  def fitness_record_params
+    params.require(:record).permit(:title, :content)
   end
 end
